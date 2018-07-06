a6c32b72e33e77e89238b0d09604c6641194caf3
JJordan003 <34778792+JJordan003@users.noreply.github.com>
2018-07-06 15:47:32 +0200

    v0.0.0

---
+++ Scripts/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody
+
+var speed = 400
+var direction = Vector3()
+
+func _ready():
+	pass
+func _physics_process(delta):
+	direction = Vector3(0, 0, 0)
+	if Input.is_action_pressed("ui_left"):
+		direction.x += 1 
+	if Input.is_action_pressed("ui_right"):
+		direction.x -= 1 
+	if Input.is_action_pressed("ui_up"):
+		direction.z += 1 
+	if Input.is_action_pressed("ui_down"):
+		direction.z -= 1
+	direction = direction.normalized()
+	direction = direction * speed * delta
+	move_and_slide(direction, Vector3(0, 1, 0))
